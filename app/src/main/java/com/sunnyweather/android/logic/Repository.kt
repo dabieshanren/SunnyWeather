b3b6455d4d05c9f0b70d44438d214f3c6050c7f4
Tony <tony@gmail.com>
2022-11-11 10:49:41 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -0,0 +1,55 @@
+package com.sunnyweather.android.logic
+
+import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.logic.model.Weather
+import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+
+object Repository {
+    fun searchPlaces(query:String) = liveData(Dispatchers.IO){
+        val result=try {
+            val placeResponse=SunnyWeatherNetwork.searchPlaces(query)
+            if (placeResponse.status == "ok"){
+                val places=placeResponse.places
+                Result.success(places)
+            }else{
+                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
+            }
+        }catch (e:Exception){
+            Result.failure<List<Place>>(e)
+        }
+        emit(result)
+    }
+    fun refreshWeather(lng:String,lat:String)=liveData(Dispatchers.IO){
+        val result=try {
+            coroutineScope {
+                val deferredRealtime=async {
+                    SunnyWeatherNetwork.getRealtimeWeather(lng,lat)
+                }
+                val deferredDaily=async {
+                    SunnyWeatherNetwork.getDailyWeather(lng,lat)
+                }
+                val realtimeResponse=deferredRealtime.await()
+                val dailyResponse=deferredDaily.await()
+                if (realtimeResponse.status == "ok" && dailyResponse.status =="ok"){
+                    val weather=Weather(realtimeResponse.result.realtime,
+                    dailyResponse.result.daily)
+                    Result.success(weather)
+                }else{
+                    Result.failure(
+                        RuntimeException(
+                            "realtime response status is ${realtimeResponse.status}"+
+                                    "daily response status is ${dailyResponse.status}"
+                        )
+                    )
+                }
+            }
+        }catch (e:Exception){
+            Result.failure<Weather>(e)
+        }
+        emit(result)
+    }
+}
